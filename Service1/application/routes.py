0e677b4d64732aa8cf1c8ace26840d7b2ebaed5b
Tom Petty <tompetty527@yahoo.co.uk>
2020-06-05 10:19:38 +0100

    Introduced the logic not fully implemented

--- Service1/application/routes.py
+++ Service1/application/routes.py
@@ -1,1 +1,11 @@
 from application import app
+from flask import render_template, request
+import requests
+import random
+
+@app.route('/', methods=['GET'])
+def home():
+    response = requests.get('http://localhost:5003/scenario')
+    print(response)
+    sentence = response.text
+    return render_template('index.html', sentence = sentence, title = 'Home')
--- Service4/application/routes.py
+++ Service4/application/routes.py
@@ -1,1 +1,32 @@
-# implement logic for stories that adress a hopeful scenario vs a bad scenario
+from application import app
+import random
+import requests
+
+
+@app.route('/randomword', methods=['GET'])
+def sentence():
+    setting = requests.get('http://localhost:5001/randomphrase')
+    noun = requests.get('http://localhost:5002/randomphrase')
+
+    a = random.randrange(2)
+
+    if a == 0:
+        return setting.text + " " + noun.text + " Happy theme"
+    else:
+        return setting.text + " " + noun.text + " Dark theme"
+
+def tempo():
+    setting = requests.get('http://localhost:5001/randomphrase')
+    noun = requests.get('http://localhost:5002/randomphrase')
+
+    a = random.randrange(2)
+
+    if a == 0:
+        return setting.text + " " + noun.text + " UPBEAT"
+    else:
+        return setting.text + " " + noun.text + " Descriptive, slow"
+
+# I want all settings and nouns to be able to pair with theme/tempo 
+# if i have to use logic based on the result of service 2 and 3 then I can have a list containing 
+# all the alphabet in CAPS (as my first letter is always caps). Then my random number genearator will decide which half of the
+# alphabet is which outcome. Thus achieving the same thing as done here so is a bit pointless. 
