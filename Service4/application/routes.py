0cb24943487bfa44aadcb14d9e23d1736fc0cce2
TomP <TomP@mail.com>
2020-06-06 20:04:11 +0000

    Fixed mistakes, first docker deployment

--- Service4/application/routes.py
+++ Service4/application/routes.py
@@ -3,28 +3,28 @@ import random
 import requests
 
 
-@app.route('/randomword', methods=['GET'])
-def sentence():
-    setting = requests.get('http://localhost:5001/randomphrase')
-    noun = requests.get('http://localhost:5002/randomphrase')
+@app.route('/theme', methods=['GET'])
+def theme():
+    setting = requests.get('http://sfia2project_service2_1:5001/randomsetting')
+    noun = requests.get('http://sfia2project_service3_1:5002/randomnoun')
 
     a = random.randrange(2)
 
     if a == 0:
-        return setting.text + " " + noun.text + " Happy theme"
+        return setting.text + ", " + noun.text + ", Happy theme"
     else:
-        return setting.text + " " + noun.text + " Dark theme"
+        return setting.text + ", " + noun.text + ", Dark theme"
 
 def tempo():
-    setting = requests.get('http://localhost:5001/randomphrase')
-    noun = requests.get('http://localhost:5002/randomphrase')
+    setting = requests.get('http://localhost:5001/randomsetting')
+    noun = requests.get('http://localhost:5002/randomnoun')
 
     a = random.randrange(2)
 
     if a == 0:
-        return setting.text + " " + noun.text + " UPBEAT"
+        return setting.text + ", " + noun.text + ", UPBEAT"
     else:
-        return setting.text + " " + noun.text + " Descriptive, slow"
+        return setting.text + ", " + noun.text + ", Descriptive, slow"
 
 # I want all settings and nouns to be able to pair with theme/tempo 
 # if i have to use logic based on the result of service 2 and 3 then I can have a list containing 
